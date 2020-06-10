7a293ed8c604dc9faebdb1023f012a28a3c50285
Joshua Groves <joshgroves@outlook.com>
2020-06-10 18:05:12 +1200

    Enviroment in with some other things

--- Dating SimSchool/Assets/Scripts/Achievements.cs
+++ Dating SimSchool/Assets/Scripts/Achievements.cs
@@ -8,6 +8,10 @@ public class Achievements : MonoBehaviour
     public GameObject KazukiImage;
     public GameObject HayateImage;
     public GameObject HideoImage;
+    public GameObject MihoImage2;
+    public GameObject KazukiImage2;
+    public GameObject HayateImage2;
+    public GameObject HideoImage2;
     public GameObject MihoImageReveal;
     public GameObject KazukiImageReveal;
     public GameObject HayateImageReveal;
@@ -18,21 +22,25 @@ public class Achievements : MonoBehaviour
         if( Image.Miho == true)
         {
             MihoImage.SetActive(true);
+            MihoImage2.SetActive(false);
             MihoImageReveal.SetActive(false);
         }
         if (Image.Kazuki == true)
         {
             KazukiImage.SetActive(true);
+            KazukiImage2.SetActive(false);
             KazukiImageReveal.SetActive(false);
         }
         if (Image.Hideo == true)
         {
             HideoImage.SetActive(true);
+            HideoImage2.SetActive(false);
             HideoImageReveal.SetActive(false);
         }
         if (Image.Hayate == true)
         {
             HayateImage.SetActive(true);
+            HayateImage2.SetActive(false);
             HayateImageReveal.SetActive(false);
         }
     }
